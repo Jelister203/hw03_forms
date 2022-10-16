1fae7cf42d07f99a5e4afd3803aa9ab22f805c36
Jelister203 <93206051+Jelister203@users.noreply.github.com>
2022-10-16 21:45:20 +0300

    fourth try

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,9 +6,9 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_posts, name='group_list'),
-    path('profile/<str:username>/', views.profile, name='profile'),
-    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('create/', views.post_create, name='post_create'),
-    path('posts/<int:post_id>/edit/', views.post_edit, name='post_create'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),  # all
+    path('profile/<str:username>/', views.profile, name='profile'),  # all
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),  # all
+    path('create/', views.post_create, name='post_create'),  # auth-only
+    path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),  # auth-only
 ]
